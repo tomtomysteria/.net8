6ef14d6ddd578dd22117a5861cd02ef92c9ad21e
Thomas CESBRON <thomas.cesbron@avec.fr>
2025-01-20 18:09:02 +0100

    feat(calculator): move class in Services folder

--- Services/Calculator.cs
+++ Services/Calculator.cs
@@ -1,13 +1,13 @@
-namespace FirstProject.Models
+namespace FirstProject.Services
 {
-    public class Calculator
+    public static class Calculator
     {
-        public int Add(int number1, int number2)
+        public static int Add(int number1, int number2)
         {
             return number1 + number2;
         }
 
-        public void PrintNumbersFrom1To10()
+        public static void PrintNumbersFrom1To10()
         {
             for (int i = 1; i <= 10; i++)
             {
